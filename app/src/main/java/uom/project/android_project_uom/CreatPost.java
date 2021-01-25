991d094c172fc07970e1fc44001bf5661a70e1e6
Angelos <aggelostsaousis19@gmail.com>
2021-01-25 21:28:05 +0200

    posting on fb

--- app/src/main/java/uom/project/android_project_uom/CreatPost.java
+++ app/src/main/java/uom/project/android_project_uom/CreatPost.java
@@ -1,49 +1,182 @@
 package uom.project.android_project_uom;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
+import android.os.Handler;
+import android.os.Parcel;
+import android.provider.MediaStore;
 import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.CheckBox;
+import android.widget.*;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import com.facebook.*;
+import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+import com.facebook.share.Share;
+import com.facebook.share.Sharer;
+import com.facebook.share.model.ShareContent;
+import com.facebook.share.model.ShareLinkContent;
+import com.facebook.share.model.SharePhoto;
+import com.facebook.share.model.SharePhotoContent;
+import com.facebook.share.widget.ShareButton;
+import com.facebook.share.widget.ShareDialog;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.FileNotFoundException;
+import java.util.Arrays;
 
 public class CreatPost extends AppCompatActivity {
 
-    CheckBox fbBox,igBox,twitBox;
+    CheckBox fbBox, igBox, twitBox;
     Button postBTN;
+    ShareButton shareBTN;
+    CallbackManager callbackManager;
+    TextView postTXT;
+    ImageView imageView, targetImage;
+    ImageButton selectImageBTN;
+
+    FacebookSdk facebook = new FacebookSdk();
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_creat_post);
+
+        //Initialize sdk
+        FacebookSdk.sdkInitialize(CreatPost.this);
 
         fbBox = findViewById(R.id.facebookCheckBox);
         igBox = findViewById(R.id.instagramCheckBox);
         twitBox = findViewById(R.id.twitterCheckBox);
-
         postBTN = findViewById(R.id.postBtn);
+        postTXT = findViewById(R.id.postText);
+        shareBTN = findViewById(R.id.shareButton);
+        imageView = findViewById(R.id.imageView6);
+        imageView.setImageResource(R.drawable.facebook);
+        selectImageBTN = findViewById(R.id.postImg);
+        targetImage = findViewById(R.id.imageView);
+
+        callbackManager = CallbackManager.Factory.create();
 
         postBTN.setOnClickListener(v -> {
+
             if (fbBox.isChecked())
             {
-
-                Log.d("demo","Post on fb");
-            }
-
-            if (igBox.isChecked())
-            {
-                Log.d("demo","Post on ig");
-            }
-
-            if (twitBox.isChecked())
-            {
-                Log.d("demo","Post on twitter");
+                facebookActivity();
             }
 
         });
 
+        selectImageBTN.setOnClickListener(v -> {
+            Intent intent = new Intent(Intent.ACTION_PICK,
+                    android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+            startActivityForResult(intent, 0);
+        });
+
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+        super.onActivityResult(requestCode, resultCode, data);
+
+
+
+
+
+
+        /*
+        Uri targetUri = data.getData();
+        Bitmap bitmap = null;
+        try {
+            bitmap = BitmapFactory.decodeStream(getContentResolver().openInputStream(targetUri));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        if (bitmap != null) {
+
+
+            SharePhoto sharePhoto = new SharePhoto.Builder()
+                    .setBitmap(bitmap)
+                    .build();
+
+            SharePhotoContent sharePhotoContent = new SharePhotoContent.Builder()
+                    .addPhoto(sharePhoto)
+                    .build();
+
+            shareBTN.setShareContent(sharePhotoContent);
+        }
+
+         */
+
+
+
     }
 
 
+    public void facebookActivity(){
+
+        LoginManager.getInstance().logInWithReadPermissions(CreatPost.this,Arrays.asList("user_gender, user_friends"));
+        LoginManager.getInstance().registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+
+            }
+
+            @Override
+            public void onCancel() {
+
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+
+            }
+        });
+
+        postOnFacebook();
+
+
+    }
+
+
+    public void postOnFacebook(){
+
+        GraphRequest graphRequest = GraphRequest.newMeRequest(AccessToken.getCurrentAccessToken(),
+                new GraphRequest.GraphJSONObjectCallback() {
+                    @Override
+                    public void onCompleted(JSONObject object, GraphResponse response) {
+
+                        Log.d("demo",response.toString());
+
+                        try {
+                            String id = object.getJSONObject("id").toString();
+                            System.out.println("id:"+id);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                });
+
+        Bundle bundle = new Bundle();
+        bundle.putString("fields","id");
+
+        graphRequest.setParameters(bundle);
+        graphRequest.executeAsync();
+    }
 }
+
+
+
+
+
--- app/src/main/java/uom/project/android_project_uom/MainActivity.java
+++ app/src/main/java/uom/project/android_project_uom/MainActivity.java
@@ -1,58 +1,19 @@
 package uom.project.android_project_uom;
 
 import android.content.Intent;
-import android.util.Log;
 import android.widget.ImageButton;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
-import com.facebook.*;
-import com.facebook.login.LoginManager;
-import com.facebook.login.LoginResult;
-import com.facebook.login.widget.LoginButton;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.util.Arrays;
-
 
 public class MainActivity extends AppCompatActivity
 {
-
-    private CallbackManager callbackManager;
-    private LoginButton loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        loginButton = findViewById(R.id.login_button);
-
-        callbackManager = CallbackManager.Factory.create();
-
-        loginButton.setPermissions(Arrays.asList("user_gender, user_friends"));
-
-        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
-            @Override
-            public void onSuccess(LoginResult loginResult) {
-                Log.d("Demo", "Succ");
-            }
-
-            @Override
-            public void onCancel() {
-                Log.d("Demo", "can");
-
-            }
-
-            @Override
-            public void onError(FacebookException error) {
-                Log.d("Demo", "Error");
-
-            }
-        });
-
 
 
         ImageButton twitterTrendsBtn = findViewById(R.id.twitterTrendsBtn);
@@ -64,63 +25,4 @@ public class MainActivity extends AppCompatActivity
 
     }
 
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        callbackManager.onActivityResult(requestCode, resultCode, data);
-        super.onActivityResult(requestCode, resultCode, data);
-
-
-
-        GraphRequest graphRequest = GraphRequest.newMeRequest(AccessToken.getCurrentAccessToken(),
-                new GraphRequest.GraphJSONObjectCallback() {
-                    @Override
-                    public void onCompleted(JSONObject object,GraphResponse response){
-                        Log.d("Demo",object.toString());
-
-                        try {
-                            String id = object.getString("id");
-
-                            //new  request
-                            /* make the API call */
-                            new GraphRequest(
-                                    AccessToken.getCurrentAccessToken(),
-                                    "/oembed_page?url=https%3A%2F%2Fwww.facebook.com%2FCDC&access_token=6a297178d0a42cda6a1ee440c41929ee",
-                                    null,
-                                    HttpMethod.GET,
-                                    response1 -> Log.d("Demo", response1.toString())
-                            ).executeAsync();
-
-                        } catch (JSONException e) {
-                            e.printStackTrace();
-                        }
-
-
-                    }
-
-                });
-
-        Bundle bundle = new Bundle();
-        bundle.putString("fields","gender, name, id, first_name, last_name");
-
-        graphRequest.setParameters(bundle);
-        graphRequest.executeAsync();
-
-    }
-
-    AccessTokenTracker accessTokenTracker = new AccessTokenTracker() {
-        @Override
-        protected void onCurrentAccessTokenChanged(AccessToken oldAccessToken, AccessToken currentAccessToken) {
-            if (currentAccessToken == null)
-            {
-                LoginManager.getInstance().logOut();
-            }
-        }
-    };
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        accessTokenTracker.stopTracking();
-    }
 }
