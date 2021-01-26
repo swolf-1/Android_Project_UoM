471973aa20e48ed5b25bdbd145022dcaf15b0ff0
Angelos <aggelostsaousis19@gmail.com>
2021-01-26 22:11:12 +0200

    Facebook photo post + log in and log out control

--- app/src/main/java/uom/project/android_project_uom/CreatPost.java
+++ app/src/main/java/uom/project/android_project_uom/CreatPost.java
@@ -1,56 +1,51 @@
 package uom.project.android_project_uom;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
 import android.os.Handler;
-import android.os.Parcel;
-import android.provider.MediaStore;
+import android.se.omapi.Session;
 import android.util.Log;
+import android.view.View;
 import android.widget.*;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import com.facebook.*;
-import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.Login;
 import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
-import com.facebook.login.widget.LoginButton;
-import com.facebook.share.Share;
-import com.facebook.share.Sharer;
-import com.facebook.share.model.ShareContent;
-import com.facebook.share.model.ShareLinkContent;
 import com.facebook.share.model.SharePhoto;
 import com.facebook.share.model.SharePhotoContent;
 import com.facebook.share.widget.ShareButton;
-import com.facebook.share.widget.ShareDialog;
-import org.json.JSONException;
-import org.json.JSONObject;
 
 import java.io.FileNotFoundException;
 import java.util.Arrays;
+import com.facebook.FacebookSdk;
+import com.facebook.share.widget.ShareDialog;
+import com.squareup.picasso.Picasso;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class CreatPost extends AppCompatActivity {
+
 
     CheckBox fbBox, igBox, twitBox;
     Button postBTN;
     ShareButton shareBTN;
     CallbackManager callbackManager;
-    TextView postTXT;
-    ImageView imageView, targetImage;
-    ImageButton selectImageBTN;
+    TextView postTXT,usersNameTXT,loggedAsTXT;
+    ImageView targetImage;
+    ImageButton selectImageBTN,log_outBTN;
+    Bitmap bitmap;
 
-    FacebookSdk facebook = new FacebookSdk();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_creat_post);
-
-        //Initialize sdk
-        FacebookSdk.sdkInitialize(CreatPost.this);
 
         fbBox = findViewById(R.id.facebookCheckBox);
         igBox = findViewById(R.id.instagramCheckBox);
@@ -58,28 +53,52 @@ public class CreatPost extends AppCompatActivity {
         postBTN = findViewById(R.id.postBtn);
         postTXT = findViewById(R.id.postText);
         shareBTN = findViewById(R.id.shareButton);
-        imageView = findViewById(R.id.imageView6);
-        imageView.setImageResource(R.drawable.facebook);
         selectImageBTN = findViewById(R.id.postImg);
         targetImage = findViewById(R.id.imageView);
+        usersNameTXT = findViewById(R.id.usersName);
+        log_outBTN = findViewById(R.id.logOutBTN);
+        loggedAsTXT = findViewById(R.id.loggedAsTXT);
+
+
 
         callbackManager = CallbackManager.Factory.create();
 
+
+        if (isFacebookLoggedIn()){
+            setUsersNameOnTop();
+        }
+        else{
+            removeUsersName();
+        }
+
+        log_outBTN.setOnClickListener(v -> {
+            LoginManager.getInstance().logOut();
+            removeUsersName();
+
+            Toast.makeText(getApplicationContext(),"You are successfully logged out.", Toast.LENGTH_LONG).show();
+
+        });
+
+
         postBTN.setOnClickListener(v -> {
 
-            if (fbBox.isChecked())
-            {
-                facebookActivity();
+            if (fbBox.isChecked()){
+
+                facebookLogin();
+
+                sharePhoto(bitmap);
+
+
             }
 
         });
 
         selectImageBTN.setOnClickListener(v -> {
-            Intent intent = new Intent(Intent.ACTION_PICK,
-                    android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+            Intent intent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
             startActivityForResult(intent, 0);
-        });
 
+
+        });
 
     }
 
@@ -89,46 +108,40 @@ public class CreatPost extends AppCompatActivity {
         super.onActivityResult(requestCode, resultCode, data);
 
 
-
-
-
-
-        /*
-        Uri targetUri = data.getData();
-        Bitmap bitmap = null;
-        try {
-            bitmap = BitmapFactory.decodeStream(getContentResolver().openInputStream(targetUri));
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
+        if (resultCode == RESULT_OK && data.getData()!= null) {
+            //For the selected photo from gallery
+            Uri targetUri = data.getData();
+            try {
+                bitmap = BitmapFactory.decodeStream(getContentResolver().openInputStream(targetUri));
+                targetImage.setImageBitmap(bitmap);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
         }
 
-        if (bitmap != null) {
+        if (isFacebookLoggedIn()){
+            setUsersNameOnTop();
 
-
-            SharePhoto sharePhoto = new SharePhoto.Builder()
-                    .setBitmap(bitmap)
-                    .build();
-
-            SharePhotoContent sharePhotoContent = new SharePhotoContent.Builder()
-                    .addPhoto(sharePhoto)
-                    .build();
-
-            shareBTN.setShareContent(sharePhotoContent);
         }
-
-         */
-
-
+        else{
+            removeUsersName();
+        }
 
     }
 
 
-    public void facebookActivity(){
+    public boolean isFacebookLoggedIn(){
+        return AccessToken.getCurrentAccessToken() != null;
+    }
+
+
+    public void facebookLogin(){
 
         LoginManager.getInstance().logInWithReadPermissions(CreatPost.this,Arrays.asList("user_gender, user_friends"));
         LoginManager.getInstance().registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
             @Override
             public void onSuccess(LoginResult loginResult) {
+
 
             }
 
@@ -143,36 +156,67 @@ public class CreatPost extends AppCompatActivity {
             }
         });
 
-        postOnFacebook();
+    }
+
+    public void setUsersNameOnTop(){
+
+        loggedAsTXT.setVisibility(View.VISIBLE);
+        GraphRequest graphRequest = GraphRequest.newMeRequest(AccessToken.getCurrentAccessToken(),
+                (object, response) -> {
+
+                    try {
+                        String name = object.getString("name");
+                        usersNameTXT.setText(name);
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+
+                });
+
+        Bundle bundle = new Bundle();
+        bundle.putString("fields", "name");
+
+        graphRequest.setParameters(bundle);
+        graphRequest.executeAsync();
+        log_outBTN.setVisibility(View.VISIBLE);
+
 
 
     }
 
+    public void removeUsersName(){
+        usersNameTXT.setText("You are NOT logged in yet.");
+        loggedAsTXT.setVisibility(View.INVISIBLE);
+        log_outBTN.setVisibility(View.INVISIBLE);
+    }
 
-    public void postOnFacebook(){
 
-        GraphRequest graphRequest = GraphRequest.newMeRequest(AccessToken.getCurrentAccessToken(),
-                new GraphRequest.GraphJSONObjectCallback() {
-                    @Override
-                    public void onCompleted(JSONObject object, GraphResponse response) {
 
-                        Log.d("demo",response.toString());
+    public void sharePhoto(Bitmap bm){
 
-                        try {
-                            String id = object.getJSONObject("id").toString();
-                            System.out.println("id:"+id);
-                        } catch (JSONException e) {
-                            e.printStackTrace();
-                        }
 
-                    }
-                });
+        SharePhoto sharePhoto = new SharePhoto.Builder()
+        .setBitmap(bm)
+        .build();
 
-        Bundle bundle = new Bundle();
-        bundle.putString("fields","id");
+        SharePhotoContent sharePhotoContent = new SharePhotoContent.Builder()
+                .addPhoto(sharePhoto)
+                .build();
 
-        graphRequest.setParameters(bundle);
-        graphRequest.executeAsync();
+        ShareDialog dialog = new ShareDialog(this);
+        dialog.show(sharePhotoContent);
+
+        /*
+
+        SharePhoto photo = new SharePhoto.Builder().setBitmap(bm).build();
+        SharePhotoContent content = new SharePhotoContent.Builder().addPhoto(photo).build();
+        ShareDialog dialog = new ShareDialog(this);
+        dialog.show(content);
+
+         */
+
+
     }
 }
 
